9276b980bdff84ae9cbc9a45d42ee08b404effeb
Camiel Luijkx <camiel.luijkx@gmail.com>
2019-03-21 16:53:22 +0100

    program flow (loop)

--- LISTING 1-54 for loops/LISTING 1-54 for loops/Program.cs
+++ LISTING 1-54 for loops/LISTING 1-54 for loops/Program.cs
@@ -2,32 +2,41 @@
 
 namespace LISTING_1_54_for_loops
 {
+    /*
+    
+    A loop that is not infinite (one that should terminate at some point) can be made up of three things:
+
+        1. Initialization that is performed to set up the loop.
+        2. A test that will determine if the loop should continue.
+        3. An update to be performed each time the action of the loop has been performed.
+        
+    */
     class Program
     {
         static int counter;
 
         static void initalize()
         {
-            Console.WriteLine("Initialize called");
+            Console.WriteLine("Initialize called.");
             counter = 0;
         }
 
         static void update()
         {
-            Console.WriteLine("Update called");
+            Console.WriteLine("Update called.");
             counter = counter + 1;
         }
 
         static bool test()
         {
-            Console.WriteLine("Test called");
+            Console.WriteLine("Test called.");
             return counter < 5;
         }
         static void Main(string[] args)
         {
-            for(initalize(); test();  update())
+            for (initalize(); test(); update())
             {
-                Console.WriteLine("Hello {0}", counter);
+                Console.WriteLine($"Hello {counter}.");
             }
             Console.ReadKey();
         }
--- LISTING 1-57 uppercase Person/LISTING 1-57 uppercase Person/Program.cs
+++ LISTING 1-57 uppercase Person/LISTING 1-57 uppercase Person/Program.cs
@@ -1,11 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LISTING_1_57_uppercase_Person
 {
+    /*
+    
+    If the foreach loop is working on a list of references to objects, the objects on the ends of those references can 
+    be changed.
+
+    The foreach construction can iterate through any object which implements the IEnumerable interface. These objects
+    expose a method called GetIterator(). This method must return an object that implements the 
+    System.Collections.IEnumerator interface. This interface exposes methods that the foreach construction can use to 
+    get the next item from the enumerator and determine if there any more items in the collection. Many collection 
+    classes, including listst and dictionaries, implement the IEnumerable interface.
+
+    Note that the iteration can be implemented in a "lazy" way; the next item to be iterated only needs to be fetched
+    when requested. The result of database queries can be returned as objects that implement the IEnumerable interface
+    and then only fetch the actual data items when needed. It is important that the item being iterated is not changed
+    during iteration, if the iterating code tried to remove items from the list it was iteraqting through this would 
+    cause the program to throw an exception when it ran.
+    
+    */
     class Program
     {
         class Person
@@ -20,16 +34,22 @@ namespace LISTING_1_57_uppercase_Person
 
         static void Main(string[] args)
         {
-            Person[] people = new Person[] {
+            Person[] people = new Person[] 
+            {
                 new Person("Rob"), new Person("Mary"),
                 new Person("David"), new Person("Jenny"),
-                new Person("Chris"), new Person("Imogen") };
+                new Person("Chris"), new Person("Imogen")
+            };
 
             foreach (Person person in people)
+            {
                 person.Name = person.Name.ToUpper();
+            }
 
             foreach (Person person in people)
+            {
                 Console.WriteLine(person.Name);
+            }
 
             Console.ReadKey();
         }
